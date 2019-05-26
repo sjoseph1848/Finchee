e1fa1e3d3c261a2ef0d1f63468a63e81ea31328a
Samuel Joseph <samueljoseph@Samuels-MBP.frontierlocal.net>
2019-05-26 10:45:53 -0500

    first commit

---
+++ Finchee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Finchee
+//
+//  Created by Samuel Joseph on 5/26/19.
+//  Copyright © 2019 Samuel Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
