89570bf8bb600d2a5b60ebc8423c622772f94fde
Samuel Joseph <samueljoseph@Samuels-MBP.frontierlocal.net>
2019-06-17 22:38:22 -0500

    attempt to resolve json connection

--- Finchee/Headlines/FincheeHeadlineCell.swift
+++ Finchee/Headlines/FincheeHeadlineCell.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Samuel Joseph. All rights reserved.
 //
 
+
 import Foundation
 import UIKit
 
@@ -16,9 +17,9 @@ private extension CGFloat {
     static let innerVerticalOffset: CGFloat = 10
 }
 
-class RobinhoodCardCell: UICollectionViewCell {
+class FincheeHeadlineCell: UICollectionViewCell {
     
-    static let identifier = "FincheeHeadlineCell"
+    static let identifier = "HeadlineCell"
     
     private let textLabel = UILabel()
     private let titleLabel = UILabel()
@@ -99,4 +100,111 @@ class RobinhoodCardCell: UICollectionViewCell {
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import Foundation
+//import UIKit
+//
+//private extension CGFloat {
+//    static let widthMultiplier: CGFloat = 0.8
+//    static let outerVerticalOffset: CGFloat = 15
+//    static let horizontalOffset: CGFloat = 25
+//    static let innerVerticalOffset: CGFloat = 10
+//}
+//
+//class FincheeHeadlineCell: UICollectionViewCell {
+//
+//    static let identifier = "HeadlineCell"
+//
+//    private let textLabel = UILabel()
+//    private let titleLabel = UILabel()
+//    private let linkLabel = UILabel()
+//    private let cardView = UIView()
+//
+//    var backgroundType: BackgroundType {
+//        didSet {
+//            textLabel.textColor = backgroundType.textTextColor
+//            titleLabel.textColor = backgroundType.titleTextColor
+//            linkLabel.textColor = backgroundType.linkTextColor
+//        }
+//    }
+//
+//    var viewModel: FincheeHeadlineViewModel? {
+//        didSet {
+//            guard let model = viewModel else { return }
+//            textLabel.configureTextLabel(withText: model.name)
+//            titleLabel.configureTitleLabel(withText: model.title)
+//            linkLabel.configureLinkLabel(withText: model.url)
+//        }
+//    }
+//
+//    override init(frame: CGRect) {
+//        backgroundType = .light(priceMovement: .up)
+//        super.init(frame: frame)
+//        setUpViews()
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//    func setUpViews() {
+//        contentView.addSubview(cardView)
+//
+//        cardView.layer.cornerRadius = 4
+//        cardView.backgroundColor = .white
+//        cardView.layer.shadowColor = UIColor.black.cgColor
+//        cardView.layer.shadowOpacity = 0.2
+//        cardView.layer.shadowRadius = 10
+//        cardView.layer.shadowOffset = CGSize(width: -1, height: 2)
+//        cardView.translatesAutoresizingMaskIntoConstraints = false
+//
+//        contentView.addConstraints([
+//            NSLayoutConstraint(item: cardView, attribute: .height, relatedBy: .equal, toItem: contentView, attribute: .height, multiplier: 0.9, constant: 0.0),
+//            NSLayoutConstraint(item: cardView, attribute: .width, relatedBy: .equal, toItem: contentView, attribute: .width, multiplier: 0.9, constant: 0.0),
+//            NSLayoutConstraint(item: cardView, attribute: .centerX, relatedBy: .equal, toItem: contentView, attribute: .centerX, multiplier: 1.0, constant: 0.0),
+//            NSLayoutConstraint(item: cardView, attribute: .centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
+//            ])
+//
+//        cardView.addSubview(titleLabel)
+//        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+//
+//        cardView.addSubview(textLabel)
+//        textLabel.translatesAutoresizingMaskIntoConstraints = false
+//
+//        cardView.addSubview(linkLabel)
+//        linkLabel.translatesAutoresizingMaskIntoConstraints = false
+//
+//        contentView.addConstraints([
+//            NSLayoutConstraint(item: titleLabel, attribute: .leading, relatedBy: .equal, toItem: cardView, attribute: .leading, multiplier: 1.0, constant: CGFloat.horizontalOffset),
+//            NSLayoutConstraint(item: titleLabel, attribute: .trailing, relatedBy: .equal, toItem: cardView, attribute: .trailing, multiplier: 1.0, constant: -CGFloat.horizontalOffset),
+//            NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: cardView, attribute: .top, multiplier: 1.0, constant: CGFloat.outerVerticalOffset)
+//            ])
+//
+//        contentView.addConstraints([
+//            NSLayoutConstraint(item: textLabel, attribute: .leading, relatedBy: .equal, toItem: titleLabel, attribute: .leading, multiplier: 1.0, constant: 0.0),
+//            NSLayoutConstraint(item: textLabel, attribute: .trailing, relatedBy: .equal, toItem: titleLabel, attribute: .trailing, multiplier: 1.0, constant: 0.0),
+//            NSLayoutConstraint(item: textLabel, attribute: .top, relatedBy: .equal, toItem: titleLabel, attribute: .bottom, multiplier: 1.0, constant: CGFloat.innerVerticalOffset),
+//            ])
+//
+//        contentView.addConstraints([
+//            NSLayoutConstraint(item: linkLabel, attribute: .leading, relatedBy: .equal, toItem: titleLabel, attribute: .leading, multiplier: 1.0, constant: 0.0),
+//            NSLayoutConstraint(item: linkLabel, attribute: .trailing, relatedBy: .equal, toItem: titleLabel, attribute: .trailing, multiplier: 1.0, constant: 0.0),
+//            NSLayoutConstraint(item: linkLabel, attribute: .bottom, relatedBy: .equal, toItem: cardView, attribute: .bottom, multiplier: 1.0, constant: -CGFloat.outerVerticalOffset)
+//            ])
+//    }
+//
+//}
 
--- Finchee/Headlines/HeadlineStackLayout.swift
+++ Finchee/Headlines/HeadlineStackLayout.swift
@@ -5,48 +5,12 @@
 //  Created by Samuel Joseph on 6/16/19.
 //  Copyright © 2019 Samuel Joseph. All rights reserved.
 //
-
 import UIKit
-
-protocol HeadlineStackLayoutDelegate:class {
-    func headlineShouldRemove(_ flowLayout: HeadlineStackLayout,indexPath:IndexPath)
-}
 
 class HeadlineStackLayout: UICollectionViewLayout {
     
-    private var panGestureRecognizer: UIPanGestureRecognizer = UIPanGestureRecognizer()
-    private let maxOffsetThresholdPercentage:CGFloat = 0.3
-    private let animationDuration: TimeInterval = 0.15
-    
-    typealias CellWithIndexPath = (cell:UICollectionViewCell,indexPath:IndexPath)
-    
-    weak var delegate:HeadlineStackLayoutDelegate?
-    
-    private var topCellWithIndexPath: CellWithIndexPath? {
-        let lastItem = collectionView?.numberOfItems(inSection: 0) ?? 0
-        let indexPath = IndexPath(item: lastItem - 1,section:0)
-        
-        guard let cell = collectionView?.cellForItem(at: indexPath) else {return nil}
-        
-        return (cell:cell,indexPath:indexPath)
-    }
-    
-    private var bottomCellWithIndexPath:CellWithIndexPath? {
-        guard let numItems = collectionView?.numberOfItems(inSection: 0),numItems > 1 else {return nil}
-        let indexPath = IndexPath(item:numItems - 2, section:0)
-        
-        guard let cell = collectionView?.cellForItem(at: indexPath) else {return nil}
-        
-        return (cell:cell,indexPath:indexPath)
-    }
-    
-    
-    
     override func prepare() {
         super.prepare()
-        
-    panGestureRecognizer.addTarget(self, action: #selector(handlePan(gestureRecognizer:)))
-    collectionView?.addGestureRecognizer(panGestureRecognizer)
     }
     
     fileprivate func indexPathsForElementsInRect(_ rect: CGRect) -> [IndexPath] {
@@ -60,92 +24,160 @@ class HeadlineStackLayout: UICollectionViewLayout {
         
         return indexPaths
     }
-    
-    @objc func handlePan(gestureRecognizer:UIPanGestureRecognizer){
-        let translation = gestureRecognizer.translation(in: collectionView)
-        
-        let xOffset = translation.x
-        let xMaxOffset = (collectionView?.frame.width ?? 0) * maxOffsetThresholdPercentage
-        
-        switch gestureRecognizer.state {
-        case .changed:
-            if let topCard = topCellWithIndexPath {
-                topCard.cell.transform = CGAffineTransform(translationX: xOffset, y: 0)
-            }
-            
-            if let bottomCard = bottomCellWithIndexPath{
-                
-                let draggingScale = 0.5 + (abs(xOffset) / (collectionView?.frame.width ?? 1) * 0.7)
-                let scale = draggingScale > 1 ? 1 :draggingScale
-                
-                bottomCard.cell.transform = CGAffineTransform(scaleX:scale,y:scale)
-                
-                bottomCard.cell.alpha = scale / 2
-            }
-        case .ended:
-            if abs(xOffset) > xMaxOffset {
-                if let topCard = topCellWithIndexPath {
-                    //animate and remove
-                    animateAndRemove(left: xOffset < 0, cell: topCard.cell, completion: {
-                        [weak self] in
-                        
-                        guard let `self` = self else {return}
-                        self.delegate?.headlineShouldRemove(self, indexPath: topCard.indexPath)
-                        
-                    })
-                }
-                if let bottomCard = bottomCellWithIndexPath{
-                    //animate into primary position
-                    animateIntoPosition(cell: bottomCard.cell)
-                }
-            } else {
-                if let topCard = topCellWithIndexPath {
-                    //animate back into primary position
-                    animateIntoPosition(cell: topCard.cell)
-                }
-            }
-        default:
-            break
-        }
-    }
-    
-    private func animateIntoPosition(cell: UICollectionViewCell){
-        UIView.animate(withDuration:animationDuration){
-            cell.transform = CGAffineTransform.identity
-            cell.alpha = 1
-        }
-    }
-    
-    private func animateAndRemove(left:Bool,cell:UICollectionViewCell,completion: (() -> ())?) {
-        let screenWidth = UIScreen.main.bounds.width
-        
-        UIView.animate(withDuration: animationDuration,animations: {
-            let xTranslateOffscreen = CGAffineTransform(translationX: left ? -screenWidth : screenWidth, y: 0)
-            cell.transform = xTranslateOffscreen
-        }) { (completed) in completion?()
-            
-        }
-    }
-    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
-        
-        attributes.frame = collectionView?.bounds ?? .zero
-        
-        var isNotTop = false
-        if let numItems = collectionView?.numberOfItems(inSection: 0),numItems > 0 {
-            isNotTop = indexPath.row != numItems - 1
-        }
-        
-        attributes.alpha = isNotTop ? 0 : 1
-        
-        return attributes
-    }
-    
-    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        let indexPaths = indexPathsForElementsInRect(rect)
-        let layoutAttributes = indexPaths.map {layoutAttributesForItem(at: $0)}
-            .filter {$0 != nil }.map {$0!}
-        
-        return layoutAttributes
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//import UIKit
+//
+//protocol HeadlineStackLayoutDelegate:class {
+//    func headlineShouldRemove(_ flowLayout: HeadlineStackLayout,indexPath:IndexPath)
+//}
+//
+//class HeadlineStackLayout: UICollectionViewLayout {
+//
+//    private var panGestureRecognizer: UIPanGestureRecognizer = UIPanGestureRecognizer()
+//    private let maxOffsetThresholdPercentage:CGFloat = 0.3
+//    private let animationDuration: TimeInterval = 0.15
+//
+//    typealias CellWithIndexPath = (cell:UICollectionViewCell,indexPath:IndexPath)
+//
+//    weak var delegate:HeadlineStackLayoutDelegate?
+//
+//    private var topCellWithIndexPath: CellWithIndexPath? {
+//        let lastItem = collectionView?.numberOfItems(inSection: 0) ?? 0
+//        let indexPath = IndexPath(item: lastItem - 1,section:0)
+//
+//        guard let cell = collectionView?.cellForItem(at: indexPath) else {return nil}
+//
+//        return (cell:cell,indexPath:indexPath)
+//    }
+//
+//    private var bottomCellWithIndexPath:CellWithIndexPath? {
+//        guard let numItems = collectionView?.numberOfItems(inSection: 0),numItems > 1 else {return nil}
+//        let indexPath = IndexPath(item:numItems - 2, section:0)
+//
+//        guard let cell = collectionView?.cellForItem(at: indexPath) else {return nil}
+//
+//        return (cell:cell,indexPath:indexPath)
+//    }
+//
+//
+//
+//    override func prepare() {
+//        super.prepare()
+//
+//    panGestureRecognizer.addTarget(self, action: #selector(handlePan(gestureRecognizer:)))
+//    collectionView?.addGestureRecognizer(panGestureRecognizer)
+//    }
+//
+//    fileprivate func indexPathsForElementsInRect(_ rect: CGRect) -> [IndexPath] {
+//        var indexPaths: [IndexPath] = []
+//
+//        if let numItems = collectionView?.numberOfItems(inSection: 0), numItems > 0 {
+//
+//            for i in 0...numItems-1 {
+//                indexPaths.append(IndexPath(item: i, section: 0))
+//            }
+//        }
+//
+//        return indexPaths
+//    }
+//
+//    @objc func handlePan(gestureRecognizer:UIPanGestureRecognizer){
+//        let translation = gestureRecognizer.translation(in: collectionView)
+//
+//        let xOffset = translation.x
+//        let xMaxOffset = (collectionView?.frame.width ?? 0) * maxOffsetThresholdPercentage
+//
+//        switch gestureRecognizer.state {
+//        case .changed:
+//            if let topCard = topCellWithIndexPath {
+//                topCard.cell.transform = CGAffineTransform(translationX: xOffset, y: 0)
+//            }
+//
+//            if let bottomCard = bottomCellWithIndexPath{
+//
+//                let draggingScale = 0.5 + (abs(xOffset) / (collectionView?.frame.width ?? 1) * 0.7)
+//                let scale = draggingScale > 1 ? 1 :draggingScale
+//
+//                bottomCard.cell.transform = CGAffineTransform(scaleX:scale,y:scale)
+//
+//                bottomCard.cell.alpha = scale / 2
+//            }
+//        case .ended:
+//            if abs(xOffset) > xMaxOffset {
+//                if let topCard = topCellWithIndexPath {
+//                    //animate and remove
+//                    animateAndRemove(left: xOffset < 0, cell: topCard.cell, completion: {
+//                        [weak self] in
+//
+//                        guard let `self` = self else {return}
+//                        self.delegate?.headlineShouldRemove(self, indexPath: topCard.indexPath)
+//
+//                    })
+//                }
+//                if let bottomCard = bottomCellWithIndexPath{
+//                    //animate into primary position
+//                    animateIntoPosition(cell: bottomCard.cell)
+//                }
+//            } else {
+//                if let topCard = topCellWithIndexPath {
+//                    //animate back into primary position
+//                    animateIntoPosition(cell: topCard.cell)
+//                }
+//            }
+//        default:
+//            break
+//        }
+//    }
+//
+//    private func animateIntoPosition(cell: UICollectionViewCell){
+//        UIView.animate(withDuration:animationDuration){
+//            cell.transform = CGAffineTransform.identity
+//            cell.alpha = 1
+//        }
+//    }
+//
+//    private func animateAndRemove(left:Bool,cell:UICollectionViewCell,completion: (() -> ())?) {
+//        let screenWidth = UIScreen.main.bounds.width
+//
+//        UIView.animate(withDuration: animationDuration,animations: {
+//            let xTranslateOffscreen = CGAffineTransform(translationX: left ? -screenWidth : screenWidth, y: 0)
+//            cell.transform = xTranslateOffscreen
+//        }) { (completed) in completion?()
+//
+//        }
+//    }
+//    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+//        let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+//
+//        attributes.frame = collectionView?.bounds ?? .zero
+//
+//        var isNotTop = false
+//        if let numItems = collectionView?.numberOfItems(inSection: 0),numItems > 0 {
+//            isNotTop = indexPath.row != numItems - 1
+//        }
+//
+//        attributes.alpha = isNotTop ? 0 : 1
+//
+//        return attributes
+//    }
+//
+//    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+//        let indexPaths = indexPathsForElementsInRect(rect)
+//        let layoutAttributes = indexPaths.map {layoutAttributesForItem(at: $0)}
+//            .filter {$0 != nil }.map {$0!}
+//
+//        return layoutAttributes
+//    }
+//}
