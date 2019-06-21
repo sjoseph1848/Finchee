e7e7845ac83693fdd7e1b6b0d20983d36ea57ff5
sjoseph1848 <sjoseph1848@gmail.com>
2019-06-21 06:15:22 -0500

    refactored the code into a method for reusability

--- Finchee/View/BaseTabBarController.swift
+++ Finchee/View/BaseTabBarController.swift
@@ -37,5 +37,17 @@ class BaseTabBarController:UITabBarController {
         blueNavController
         ]
         
+        
+    }
+    fileprivate func createNavController(viewController:UIViewController,title:String,imageName:String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: viewController)
+        
+        navController.navigationBar.prefersLargeTitles = true
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .white
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named:imageName)
+        
+        return navController
     }
 }
